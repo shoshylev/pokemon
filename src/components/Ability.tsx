beaace5f24bf6aa7d4b2c7ffbc255ddb0b84ac49
Shoshi Rosental <srosental@planview.com>
2024-01-02 23:05:24 +0200

    review

--- src/components/Ability.tsx
+++ src/components/Ability.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 
-export default function Ability({displayName}:{name?:string}) {
+export default function Ability({displayName}:{displayName?:string}) {
   const { name } = useParams();
   return <div>{name || displayName}</div>;
 }
